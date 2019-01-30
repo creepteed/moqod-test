7a4fe8ad1e981209fdca7f6de0ed601389daeaa0
creepteed <creepteed@gmail.com>
2019-01-30 21:09:58 +0200

    updt

--- src/components/Table/CurrencyRow/CurrencyRow.vue
+++ src/components/Table/CurrencyRow/CurrencyRow.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="curr-row">
         <span class="curr-row__title" :class="{'highlighted': highlight}">{{currency}}: </span>
-        <span class="curr-row__value" :class="{'highlighted': highlight}">{{rate}}</span>
+        <span class="curr-row__value" :class="{'highlighted': highlight}">{{rate.toFixed(5)}} [ {{(1/rate).toFixed(2)}} {{base_currency}} ]</span>
         <div class="curr-row__update" @click="updateHandler"></div>
     </div>
 </template>
 
 <script>
+  import { mapState } from "vuex";
+
   export default {
     name: "CurrencyRow",
     props: ["currency", "rate"],
+    computed: {
+      ...mapState(["base_currency"])
+    },
     data(){
       return {
         highlight: false
